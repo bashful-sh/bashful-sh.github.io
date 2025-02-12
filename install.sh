a4ee53419cd302034b9ecc527d858db7903ab7ce
Owen Easter <owen@easter.company>
2025-02-12 00:55:25 +0000

    fix: changed source directory

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Download
-mkdir -p ~/.bashful
-git clone git@github.com:bashful-sh/bashful.git ~/.bashful/src
+sudo rm -rf ~/.bashful
+git clone git@github.com:bashful-sh/bashful.git ~/.bashful
 
 # Prepare
-sudo rm -rf /opt/bashful/
-sudo mv ~/.bashful /opt/bashful
-sudo rm -rf /opt/bashful/.git
-sudo rm -rf /opt/bashful/.gitignore
-sudo rm -rf /opt/bashful/.README.md
+sudo rm -rf ~/.bashful/.git
+sudo rm -rf ~/.bashful/.gitignore
+sudo rm -rf ~/.bashful/.README.md
 
 # Install
 sudo rm ~/.bashrc
-sudo cp /opt/bashful/src/.bashrc ~/.bashrc
+sudo mv ~/.bashful/.bashrc ~/.bashrc
 sudo chown $USER:$USER ~/.bashrc
 
 # Start
