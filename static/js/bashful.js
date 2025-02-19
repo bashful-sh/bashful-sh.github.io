579447761bb7bdd73a40a553ed6e5748967777e7
Owen Easter <owen@easter.company>
2025-02-19 02:46:11 +0000

    add: sync description field with github description

--- static/js/bashful.js
+++ static/js/bashful.js
@@ -1,5 +1,5 @@
-/*  static/js/cmd.js
- *  handles any download and install clone-snippet features.
+/*  static/js/bashful.js
+ *  handles bashful project specific functionality.
  */
 const cmdSnips = [
   `curl -fsSL https://bashful-sh.github.io/install.sh | bash`,
@@ -31,7 +31,7 @@ const bashfulSelectClone = () => {
 const bashfulOwner = "bashful-sh";
 const bashfulName = "bashful";
 const bashfulGithubAPIUrl = `https://api.github.com/repos/${bashfulOwner}/${bashfulName}`;
-const bashfulSetElement = (id, text) => document.getElementById(id).textContent = ` ${text}`;
+const bashfulSetElement = (id, text) => document.getElementById(`bashful-${id}`).textContent = ` ${text}`;
 
 fetch(bashfulGithubAPIUrl)
   .then(response => response.json())
@@ -39,17 +39,19 @@ fetch(bashfulGithubAPIUrl)
     console.log(
       `%c ${bashfulOwner}/${bashfulName} Github API Status: %c Good `,
       `background: #ddd; color: #000; padding: 4px; border-radius: 2px;`,
-      `background: #6f6; color: #000; padding: 4px; border-radius: 2px; margin-left: 1ch;`
+      `background: #6f6; color: #000; padding: 4px; border-radius: 2px; margin-left: 1ch;`,
+      data
     );
-    console.log(data);
+    const description = data.description;
     const starCount = data.stargazers_count;
     const watcherCount = data.watchers_count;
     const forkCount = data.forks_count;
     const lastUpdate = new Date(data.updated_at);
-    bashfulSetElement("bashful-star-count", starCount);
-    bashfulSetElement("bashful-watcher-count", watcherCount);
-    bashfulSetElement("bashful-fork-count", forkCount);
-    bashfulSetElement("bashful-last-updated", lastUpdate.toLocaleString());
+    bashfulSetElement("description", description);
+    bashfulSetElement("star-count", starCount);
+    bashfulSetElement("watcher-count", watcherCount);
+    bashfulSetElement("fork-count", forkCount);
+    bashfulSetElement("last-updated", lastUpdate.toLocaleString());
   })
   .catch(error => {
     console.log(
@@ -58,9 +60,8 @@ fetch(bashfulGithubAPIUrl)
       `background: #f66; color: #000; padding: 4px; border-radius: 2px;`,
       `margin-left: 1ch;`
     );
-    console.error(`Error fetching repo info @ ${bashfulGithubAPIUrl}:`, error);
-    setElement("bashful-star-count", "n/a");
-    setElement("bashful-watcher-count", "n/a");
-    setElement("bashful-fork-count", "n/a");
-    setElement("bashful-last-updated", "n/a");
+    bashfulSetElement("star-count", "n/a");
+    bashfulSetElement("watcher-count", "n/a");
+    bashfulSetElement("fork-count", "n/a");
+    bashfulSetElement("last-updated", "n/a");
   });
